1ef37e00e6817475ffd8e3bdd788ac99babc5268
Andre <andso041@student.liu.se>
2020-11-09 13:28:34 +0100

    Added comments and updated structure

--- Termostat_main/Termostat_main.ino
+++ Termostat_main/Termostat_main.ino
@@ -11,30 +11,45 @@
 #define DHTTYPE DHT22
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-void setup() {
+// ----- Setup -----
+void setup() 
+{
+  // Pullup resistor for data signal
   pinMode(2, INPUT_PULLUP);
 
   // Begin streams
   Serial.begin(9600);
   dht.begin();
-  Serial.println(F("Termostat is running!"));
+
+  // Init print
+  Serial.println(F("Termostat is now running:"));
 }
 
-void loop() {
+// ----- Loop -----
+void loop() 
+{
   // Variables
   float currTemp;
-  float calibration = 23.8 - 22.8; // DHT22 - Fluke // 2020-11-09
   sensors_event_t event;
+
+  // Calibration (DHT22 - Fluke)
+  float calibration = 23.8 - 22.8;  // 2020-11-09
   
-  // Get temperature and print.
+  // Get temperature
   dht.temperature().getEvent(&event);
-  
+
+  // Save and print temperature
   if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
+    Serial.println(F("Error reading temperature"));
   }
   else {
+    // Update reading using calibration
     currTemp = event.temperature - calibration;
+
+    // Print temperature
     Serial.println(currTemp);
   }
+
+  // Wait for next cycle
   delay(5000);
 }
