2686e7dd7fc18b7e653e420191d9d7b3a7d98c5e
Andre <andso041@student.liu.se>
2020-11-09 13:18:34 +0100

    Added DHT22 sensor code and calibration

--- Termostat_main/Termostat_main.ino
+++ Termostat_main/Termostat_main.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+// Includes Temperature
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
+// Inculdes Servo
+#include <Servo.h>
+
+// DHT Setup
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+
+  // Begin streams
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println(F("Termostat is running!"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Variables
+  float currTemp;
+  float calibration = 23.8 - 22.8; // DHT22 - Fluke // 2020-11-09
+  sensors_event_t event;
+  
+  // Get temperature and print.
+  dht.temperature().getEvent(&event);
+  
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    currTemp = event.temperature - calibration;
+    Serial.println(currTemp);
+  }
+  delay(5000);
 }
