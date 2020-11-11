7e978846ff941afa48c44a73af13af7769435bb1
Andre <andso041@student.liu.se>
2020-11-11 15:14:14 +0100

    Added function for system identification

---
+++ Systemidentifiering/Systemidentifiering.ino
@@ -0,0 +1,75 @@
+/*
+  ------------------------------
+    System Identification (Step Response)
+    Verision: 1.0
+  ------------------------------
+*/
+
+// Includes Temperature
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+// Inculdes Servo
+#include <Servo.h>
+
+// MG995 Setup
+Servo servoT;
+#define MGPIN 9
+
+// DHT22 Setup
+#define DHTPIN 12
+#define DHT5V 13
+#define DHTTYPE DHT22
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+// ---- Global variables ----
+float currTemp;
+float calibration = 23.10 - 22.7; // DHT - Fluke  // 2020-11-09
+
+// ---------- Setup ----------
+void setup() 
+{
+  // Begin Serial
+  Serial.begin(9600);
+  
+  // Pullup resistor for data signal
+  pinMode(DHTPIN, INPUT_PULLUP);
+
+  // 5V Pin to Sensor
+  pinMode(DHT5V, OUTPUT);
+
+  // Begin Sensor
+  dht.begin();
+
+  // Add and move to off
+  servoT.attach(MGPIN);
+  servoT.write(180);
+  delay(1000);
+
+  // Start test
+  servoT.write(0);
+}
+
+// ---------- Loop ----------
+void loop() 
+{
+  // Get temperature
+  digitalWrite(DHT5V, HIGH);
+  delay(1000);
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  digitalWrite(DHT5V, LOW);
+
+  // Save and print temperature
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature"));
+  }
+  else {
+    // Update reading using calibration
+    currTemp = event.temperature - calibration;
+
+    // Print temperature
+    Serial.println(currTemp);
+  }
+}
